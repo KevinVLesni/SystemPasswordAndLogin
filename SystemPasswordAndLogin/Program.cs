50ff570d2daa63f04d466e6ca78d7d08c7dabcc4
KevinVLesni <krihagosickmatron@gmail.com>
2023-06-01 12:32:29 +0300

    Добавьте файлы проекта.

---
+++ SystemPasswordAndLogin/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemPasswordAndLogin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "123qwe";
+            string UserInput;
+
+            Console.Write("Введите ваш пароль:");
+
+            UserInput = Console.ReadLine();
+
+            if(UserInput == password)
+            {
+                Console.WriteLine("Пароль принят. Доступ к базе данных разрешён.");
+            }
+            else
+            {
+                Console.WriteLine("ОШИБКА: Пароль некорректен! Доступ закрыт.");
+            }
+            Console.Read();
+        }
+    }
+}
